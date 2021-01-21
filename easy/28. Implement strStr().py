33623cca771b53952d6a382ebacd0333d1a2492b
Jaak Lin <linjaak@gmail.com>
2021-01-21 10:38:20 +0100

    Add 28

---
+++ easy/28. Implement strStr().py
@@ -0,0 +1,24 @@
+'''
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Example 3:
+Input: haystack = "", needle = ""
+Output: 0
+
+SOL: Brute force
+'''
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        return haystack.find(needle)
