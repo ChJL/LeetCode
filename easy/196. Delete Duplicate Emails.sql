a6388ec51a8ba4cf27fbcfe296769bf0bdb88e26
Jaak Lin <linjaak@gmail.com>
2021-02-08 23:35:57 +0100

    Add 196

---
+++ easy/196. Delete Duplicate Emails.sql
@@ -0,0 +1,9 @@
+#196. Delete Duplicate Emails
+
+# SOL : https://leetcode.com/problems/delete-duplicate-emails/discuss/55553/Simple-Solution
+
+# Write your MySQL query statement below
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.Email = p2.Email
+AND p1.Id > p2.Id
